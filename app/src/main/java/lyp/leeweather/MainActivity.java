936baa052034ba3cea1188686371196425c5766d
lyp <330813737@qq.com>
2017-03-27 21:31:10 +0800

    Initial commit

---
+++ app/src/main/java/lyp/leeweather/MainActivity.java
@@ -0,0 +1,14 @@
+package lyp.leeweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
